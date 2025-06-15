d91dcb6c348ccac37f1604b03f2a3ecaebc2c857
Wooksang Ryu <today8934@gmail.com>
2025-06-16 08:24:00 +0900

    Remove Duplicated

---
+++ src/main/java/com/example/algorithm/arrayandstring/RemoveDuplicated.java
@@ -0,0 +1,84 @@
+package com.example.algorithm.arrayandstring;
+
+import java.util.Arrays;
+
+/**
+ * Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same. Then return the number of unique elements in nums.
+ *
+ * Consider the number of unique elements of nums to be k, to get accepted, you need to do the following things:
+ *
+ * Change the array nums such that the first k elements of nums contain the unique elements in the order they were present in nums initially. The remaining elements of nums are not important as well as the size of nums.
+ * Return k.
+ * Custom Judge:
+ *
+ * The judge will test your solution with the following code:
+ *
+ * int[] nums = [...]; // Input array
+ * int[] expectedNums = [...]; // The expected answer with correct length
+ *
+ * int k = removeDuplicates(nums); // Calls your implementation
+ *
+ * assert k == expectedNums.length;
+ * for (int i = 0; i < k; i++) {
+ *     assert nums[i] == expectedNums[i];
+ * }
+ * If all assertions pass, then your solution will be accepted.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: nums = [1,1,2]
+ * Output: 2, nums = [1,2,_]
+ * Explanation: Your function should return k = 2, with the first two elements of nums being 1 and 2 respectively.
+ * It does not matter what you leave beyond the returned k (hence they are underscores).
+ * Example 2:
+ *
+ * Input: nums = [0,0,1,1,1,2,2,3,3,4]
+ * Output: 5, nums = [0,1,2,3,4,_,_,_,_,_]
+ * Explanation: Your function should return k = 5, with the first five elements of nums being 0, 1, 2, 3, and 4 respectively.
+ * It does not matter what you leave beyond the returned k (hence they are underscores).
+ *
+ *
+ * Constraints:
+ *
+ * 1 <= nums.length <= 3 * 104
+ * -100 <= nums[i] <= 100
+ * nums is sorted in non-decreasing order.
+ */
+public class RemoveDuplicated {
+
+    public int removeDuplicates(int[] nums) {
+        if (nums.length == 1) {
+            return 1;
+        }
+
+        int writeIdx = 1;
+
+        for (int readIdx = 1; readIdx < nums.length; readIdx++) {
+            if (nums[readIdx] != nums[readIdx - 1]) {
+                nums[writeIdx] = nums[readIdx];
+                writeIdx++;
+            }
+        }
+
+        return writeIdx;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[10];
+        nums[0] = 0;
+        nums[1] = 0;
+        nums[2] = 1;
+        nums[3] = 1;
+        nums[4] = 1;
+        nums[5] = 2;
+        nums[6] = 2;
+        nums[7] = 3;
+        nums[8] = 3;
+        nums[9] = 4;
+        RemoveDuplicated removeDuplicated = new RemoveDuplicated();
+        System.out.println(removeDuplicated.removeDuplicates(nums));
+        System.out.println(Arrays.toString(nums));
+    }
+}
